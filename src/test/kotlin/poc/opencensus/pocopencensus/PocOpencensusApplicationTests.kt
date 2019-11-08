eff6bd1e87ec0062f7ce05fe212e596d6c3f271c
Lionel ANDRE <lionel.andre@gmail.com>
2019-11-09 00:41:33 +0100

    Initial commit

---
+++ src/test/kotlin/poc/opencensus/pocopencensus/PocOpencensusApplicationTests.kt
@@ -0,0 +1,13 @@
+package poc.opencensus.pocopencensus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocOpencensusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
